6b66295ed5fdefc59d7ff7473a609d3c3095fa4d
Alexey <alexey.yus@mail.ru>
2023-09-09 12:52:25 +0300

    start position

---
+++ src/components/app/App.jsx
@@ -0,0 +1,14 @@
+// import {lazy, Suspense} from 'react'
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
+const App = () => {
+  return (
+    <div className="app">
+      <main>
+        
+      </main>
+    </div>
+  )
+}
+
+export default App
