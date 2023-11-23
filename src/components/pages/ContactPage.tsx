c3fad65aa23764db37fad60d91fdf0bdf7920d37
Alexey <alexey.yus@mail.ru>
2023-11-23 19:42:57 +0300

    fix: removed the cl attribute from the map's iframe

--- src/components/pages/ContactPage.tsx
+++ src/components/pages/ContactPage.tsx
@@ -2,7 +2,7 @@ const ContactPage = () => {
   return (
     <section className="">
       <div className="container">
-        <iframe className="w-full h-96 rounded-2xl" src="https://yandex.ru/map-widget/v1/?um=constructor%3Aae346d3b29954bada9def805fb08e1ce6b2e3a27de6522f83b54422ae9dd3b80&amp;source=constructor" cl="true"></iframe>
+        <iframe className="w-full h-96 rounded-2xl" src="https://yandex.ru/map-widget/v1/?um=constructor%3Aae346d3b29954bada9def805fb08e1ce6b2e3a27de6522f83b54422ae9dd3b80&amp;source=constructor"></iframe>
         <address className="mt-11">
           <a href="tel:+84993918449" className="
               text-[38px] 
